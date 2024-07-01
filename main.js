fc1ab18616ae10ddd828b6b038a1f38328fb6c21
Servant Clément <clementServant@protonmail.com>
2024-07-01 21:25:19 +0200

    Modification du HTML, de CSS et debut du JS

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const Max  = 500;
+
+class Game {
+    constructor() {
+        this.targetNumber = getRandomNumber(0, Max);
+        this.attempts = 0;
+    }
+
+    init() {
+        this.element = document.querySelector('.play-app');
+        this.element.classList.remove('hidden');
+        this.element.style.display = 'flex';
+    }
+}
+
+let game = null;
+const startGame = () => {
+    const startContainer = document.querySelector('#start-container');
+    startContainer.classList.add('hidden');
+
+    if (game) {
+        // Reset the game
+    }
+
+    game = new Game();
+    game.init()
+}
+
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', startGame)
+
 
